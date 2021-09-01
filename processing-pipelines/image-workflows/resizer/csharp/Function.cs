6239d149969e84090f0d47aab761909ff6939d1e
Mete Atamel <atamel@google.com>
2021-09-01 17:47:55 +0100

    Image Processing Pipeline v2 - Resizer function #28

---
+++ processing-pipelines/image-workflows/resizer/csharp/Function.cs
@@ -0,0 +1,87 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+using Google.Cloud.Functions.Framework;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Newtonsoft.Json;
+using System;
+using Google.Cloud.Storage.V1;
+using Common;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace Resizer
+{
+    public class Function : IHttpFunction
+    {
+        private const int ThumbWidth = 400;
+        private const int ThumbHeight = 400;
+
+        private readonly ILogger _logger;
+
+        private readonly string _outputBucket;
+
+        private readonly HttpRequestReader _requestReader;
+
+        public Function(ILogger<Function> logger)
+        {
+            _logger = logger;
+            var configReader = new ConfigReader(logger);
+            _outputBucket = configReader.Read("BUCKET");
+            _requestReader = new HttpRequestReader(logger);
+        }
+
+        public async Task HandleAsync(HttpContext context)
+        {
+            _logger.LogInformation("Function received request");
+
+            try
+            {
+                var (bucket, file) = await _requestReader.ReadCloudStorageData(context);
+
+                using (var inputStream = new MemoryStream())
+                {
+                    var client = await StorageClient.CreateAsync();
+                    await client.DownloadObjectAsync(bucket, file, inputStream);
+                    _logger.LogInformation($"Downloaded '{file}' from bucket '{bucket}'");
+
+                    using (var outputStream = new MemoryStream())
+                    {
+                        inputStream.Position = 0; // Reset to read
+                        using (Image image = Image.Load(inputStream))
+                        {
+                            image.Mutate(x => x
+                                .Resize(ThumbWidth, ThumbHeight)
+                            );
+                            _logger.LogInformation($"Resized image '{file}' to {ThumbWidth}x{ThumbHeight}");
+
+                            image.SaveAsPng(outputStream);
+                        }
+
+                        var outputObjectName = $"{Path.GetFileNameWithoutExtension(file)}-{ThumbWidth}x{ThumbHeight}.png";
+                        await client.UploadObjectAsync(_outputBucket, outputObjectName, "image/png", outputStream);
+                        _logger.LogInformation($"Uploaded '{outputObjectName}' to bucket '{_outputBucket}'");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error processing: " + e.Message);
+                throw e;
+            }
+        }
+    }
+}

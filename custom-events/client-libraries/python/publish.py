c874433f8fe19233fbfb9297279e50a29fe21d6d
Mete Atamel <atamel@google.com>
2022-04-26 18:22:22 +0100

    Typo and copyright header

--- custom-events/client-libraries/python/publish.py
+++ custom-events/client-libraries/python/publish.py
@@ -1,3 +1,17 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sys
 import argparse
 import json
@@ -37,7 +51,7 @@ def main():
   event = spec_pb2.CloudEvent()
   event.id = args.event_id
   event.source = args.event_source
-  # The spec value must by '1.0'
+  # The spec value must be '1.0'
   event.spec_version = '1.0'
   event.type = args.event_type
   event.text_data = args.event_data
